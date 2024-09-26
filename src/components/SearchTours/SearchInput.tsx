370b5018c25c86d04aa741842f02425b3432e0ea
Yohannes444 <115734501+Yohannes444@users.noreply.github.com>
2024-09-26 12:26:01 +0300

    add tages

--- src/components/SearchTours/SearchInput.tsx
+++ src/components/SearchTours/SearchInput.tsx
@@ -77,6 +77,14 @@ const SearchInput: React.FC<SearchInputProps> = ({ label, placeholder }) => {
         {suggestions.map((item, index) => (
           <li key={index} className="p-2 " onClick={() => handleSuggestionClick(item.destinationName)}>
             <div className="">{item.destinationName} </div>
+            {item.tags.map((tag:any, index:any) => (
+              <div key={index} className="inline-block mb-2">
+                <p className="bg-green-400 text-white py-1 px-3 rounded-md shadow-md hover:bg-green-500 transition duration-300">
+                  {tag.tagName}
+                </p>
+              </div>
+            ))}
+
             <div className="text-sm text-gray-500">
             </div>
           </li>
